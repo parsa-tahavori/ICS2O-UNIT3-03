a9ad4d51be7377c413fb2f8564eeb8a926c9d5b0
parsa-tahavori <parsa-tahavori@users.noreply.github.com>
2022-11-06 15:39:19 +0000

    Prettified Code!

--- js/script.js
+++ js/script.js
@@ -4,16 +4,16 @@
 // Created on: Nov 2022
 // This file contains the JS functions for index.html
 
-'use strict'
+"use strict"
 /**
  * This function calculates the volume of a sphere.
  */
-function calculate () {
+function calculate() {
   // input
   const radius = parseInt(document.getElementById("radius").value)
   // process
-  const volume = 4/3 * Math.PI * Math.pow(radius, 3)
+  const volume = (4 / 3) * Math.PI * Math.pow(radius, 3)
   // output
   document.getElementById("volume").innerHTML =
-    "The volume is: " + volume.toFixed(2) + ' cm³' 
+    "The volume is: " + volume.toFixed(2) + " cm³"
 }
